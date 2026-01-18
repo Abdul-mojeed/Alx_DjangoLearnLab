2b02269d5724d4a3f2a824cfea643e86232a79e7
Abdulhaleem Abdulmojeed <haleemmojeed@gmail.com>
2026-01-18 21:09:43 +0100

    Implement role-based access control with UserProfile

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,28 +1,31 @@
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import render, redirect
 
 
-# User registration view
-def register(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)   # REQUIRED by checker
-            return redirect("list_books")
-    else:
-        form = UserCreationForm()
-
-    return render(request, "relationship_app/register.html", {"form": form})
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 
 
-# Login view
-class UserLoginView(LoginView):
-    template_name = "relationship_app/login.html"
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
 
 
-# Logout view
-class UserLogoutView(LogoutView):
-    template_name = "relationship_app/logout.html"
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
